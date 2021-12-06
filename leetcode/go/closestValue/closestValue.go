6bac48444824c0f4db05b42d7772966b6801b8ef
Movsar Makhmutov <melkoto@mail.ru>
2021-12-06 11:34:29 +0300

    270

--- leetcode/go/closestValue/closestValue.go
+++ leetcode/go/closestValue/closestValue.go
@@ -8,28 +8,27 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-var result int
-
 func closestValue(root *TreeNode, target float64) int {
-	dfs(root, result, math.MaxFloat64, target)
+	var result int
+	helper(root, &result, math.MaxFloat64, target)
 	return result
 }
 
-func dfs(root *TreeNode, res int, temp, target float64) {
+func helper(root *TreeNode, res *int, temp, target float64) {
 	if root == nil {
 		return
 	}
 
 	if math.Abs(float64(root.Val) - target) < temp {
 		temp = math.Abs(float64(root.Val) - target)
-		res = root.Val
+		*res = root.Val
 	}
 
 	if target < float64(root.Val) {
-		dfs(root.Left, res, temp, target)
+		helper(root.Left, res, temp, target)
 	}
 
 	if target > float64(root.Val) {
-		dfs(root.Right, res, temp, target)
+		helper(root.Right, res, temp, target)
 	}
 }

b7ee6c838f7449582ef114770f7389ff50b5b237
Movsar Makhmutov <m.movsar@gmail.com>
2022-05-01 19:53:15 +0300

    findMaxAverage

---
+++ leetcode/js/findMaxAverage.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+};
+
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4))
+console.log(findMaxAverage([5], 1))
